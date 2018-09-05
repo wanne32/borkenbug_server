d9aec89b755619ac29340fd2664ae42ebd22e742
wanne@DebianGTX960 <wanne@DebianGTX960>
2018-09-06 00:22:28 +0200

    unescape

--- fcgi/fcgi.c
+++ fcgi/fcgi.c
@@ -4,10 +4,10 @@
 #include <syslog.h>
 #include <string.h>
 #include <locale.h>
+#include <curl/curl.h>
 
 
-
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
   setlocale(LC_ALL, "C.UTF-8");
   regex_t rgex;
@@ -17,29 +17,41 @@ int main(int argc, char *argv)
     syslog(LOG_ERR,"Broken regex!\n");
     exit(1);
   }
+  
+  CURL *curl = curl_easy_init();
+  if(!curl)
+  {
+    syslog(LOG_ERR,"Can not initialize curl");
+    return 1;
+  }
+      
   FILE* file; 
   file=fopen("/var/www/data/data","a");
   
   if(!file)
   {
-    syslog(LOG_NOTICE,"Can not open file /var/www/data/data");
+    syslog(LOG_ERR,"Can not open file /var/www/data/data");
     return 1;
   }
   while (FCGI_Accept() >= 0) {
-      
-      int status = regexec(&rgex, getenv("QUERY_STRING"), (size_t) 0, NULL, 0);
-      if(status)
-      {
-          printf("Content-type: text/plain\r\n\r\n Wrong Format");
-      }else
-      {
-          strlen(getenv("QUERY_STRING"));
-          int ret= fputs(getenv("QUERY_STRING"),file);
-          fflush(file);
-          if(ret!=EOF)
-              printf("Content-type: text/plain\r\n\r\n successfully added \n%s",getenv("QUERY_STRING"));
-      }
-	}
-	return (0);
+    char* dataset=curl_easy_unescape(curl,getenv("QUERY_STRING"),0,NULL);
+    if(!dataset)
+      syslog(LOG_NOTICE,"urlunescape failed.");
+    
+    int status = regexec(&rgex, dataset, (size_t) 0, NULL, 0);
+    if(status)
+    {
+      printf("Content-type: text/plain\r\n\r\n Wrong Format");
+    }
+    else
+    {
+      int ret=fputs(dataset,file);
+      fflush(file);
+      if(ret!=EOF)
+        printf("Content-type: text/plain\r\n\r\n successfully added %d Bytes:\n%s",ret,dataset);
+    }
+    curl_free(dataset);
+  }
+  return(0);
 }
 
